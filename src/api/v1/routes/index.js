84b8625430eb8f778fcb3d105456b0826053cf03
Rafael E Quintero <raf4.quintero@gmail.com>
2023-02-28 22:11:59 -0600

    add env-example

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -5,7 +5,7 @@ import commentRouter from './comment.router'
 
 export default function routes(app) {
   app.use('/api/v1/users', userRouter)
-  app.use('/api/v1/posts', postRouter)
+  // app.use('/api/v1/posts', postRouter)
   app.use('/api/v1/comments', commentRouter)
   // app.use('/api/v1/signup', registerRouter)
 }
