b2c54e8505e3232c8e41b7d7c8d532766bc28aba
Rafael E Quintero <raf4.quintero@gmail.com>
2023-02-28 22:10:34 -0600

    add env-example

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -7,5 +7,5 @@ export default function routes(app) {
   app.use('/api/v1/users', userRouter)
   app.use('/api/v1/posts', postRouter)
   app.use('/api/v1/comments', commentRouter)
-  app.use('/api/v1/signup', registerRouter)
+  // app.use('/api/v1/signup', registerRouter)
 }
